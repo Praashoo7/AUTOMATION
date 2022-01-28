41a4b2210658862e671414271a8fc8c3ebb17958
Prashant Patil <rnttl90@gmail.com>
2022-01-28 18:34:13 +0530

    Create Ext.py

---
+++ FileHandling/1/Ext.py
@@ -0,0 +1,29 @@
+from sys import *
+import display as d
+
+def main():
+	
+    print("\n----DISPLAY FILE-----\n")
+    
+    if len(argv)>3 or len(argv)<2:
+        print("INVALID ARGUMENT\nEnter -u for Usage\nEnter -h for Help")
+    
+    if len(argv)==2:
+        if argv[1]=="-u" or argv[1]=="-U":
+            print("USAGE : This is used to Displya all Files with Given Extension.")
+        elif argv[1]=="-h" or argv[1]=="-H":
+            print("HELP : DIRECTORY_NAME EXTENSION")
+        else:
+            print("No such Flag Exists")
+    
+    if len(argv)==3:
+        try:
+            dname=argv[1]
+            ename=argv[2]
+            d.display(dname,ename)
+        except Exception as c:
+            print(c)
+            quit()
+
+if __name__=="__main__":
+	main()
